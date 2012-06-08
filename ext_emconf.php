a2e072da6190b14d46399e487e61c198fdb2e6a2
Lorenz <lorenz.ulrich@visol.ch>
2012-06-08 12:16:03 +0200

    [TASK] Raising version to 1.1.0dev after release
    Change-Id: Iac7e062e3c7c46c7a70c84ad5456fedbc4f8ee62

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "svmetaextract".
 #
-# Auto generated 04-10-2011 17:07
+# Auto generated 08-06-2012 12:06
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0dev',
+	'version' => '1.1.0dev',
 	'constraints' => array(
 		'depends' => array(
 			'static_info_tables' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"cc1c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"999b";s:14:"ext_tables.php";s:4:"d4e6";s:19:"doc/wizard_form.dat";s:4:"280a";s:20:"doc/wizard_form.html";s:4:"5c66";s:34:"lib/class.tx_svmetaextract_lib.php";s:4:"7fc0";s:34:"sv1/class.tx_svmetaextract_sv1.php";s:4:"5702";s:34:"sv2/class.tx_svmetaextract_sv2.php";s:4:"f7e7";s:34:"sv3/class.tx_svmetaextract_sv3.php";s:4:"5b8b";s:34:"sv4/class.tx_svmetaextract_sv4.php";s:4:"1668";s:34:"sv5/class.tx_svmetaextract_sv5.php";s:4:"6b7b";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"0659";s:16:"ext_autoload.php";s:4:"9dc5";s:12:"ext_icon.gif";s:4:"999b";s:14:"ext_tables.php";s:4:"aeea";s:14:"doc/manual.sxw";s:4:"5387";s:19:"doc/wizard_form.dat";s:4:"280a";s:20:"doc/wizard_form.html";s:4:"5c66";s:34:"lib/class.tx_svmetaextract_lib.php";s:4:"6b32";s:34:"sv1/class.tx_svmetaextract_sv1.php";s:4:"3755";s:34:"sv2/class.tx_svmetaextract_sv2.php";s:4:"9890";s:34:"sv3/class.tx_svmetaextract_sv3.php";s:4:"d1bd";s:34:"sv4/class.tx_svmetaextract_sv4.php";s:4:"2b13";s:34:"sv5/class.tx_svmetaextract_sv5.php";s:4:"8232";}',
 	'suggests' => array(
 	),
 );
